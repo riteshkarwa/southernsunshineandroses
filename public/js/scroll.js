3e606a9f0104f1a71901c6780bf97f485a3ca702
ritesh karwa <riteshkarwa@riteshs-MBP.attlocal.net>
2016-04-16 17:23:40 -0700

    removed the data toggle from li tag

--- public/js/scroll.js
+++ public/js/scroll.js
@@ -1,4 +1,9 @@
 $(document).ready(function(){
+  $('.nav a').on('click', function(){
+    $('.btn-navbar').click(); //bootstrap 2.x
+    $('.navbar-toggle').click() //bootstrap 3.x by Richard
+  });
+
 	$('#nav-wrapper').height($("#nav").height());
 	$('#nav').affix({
         offset: { top: $('#nav').offset().top }
