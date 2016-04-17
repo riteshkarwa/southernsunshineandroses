09ff0c84a8cadf9b314bf3cf844f7fbef43b247a
ritesh karwa <riteshkarwa@riteshs-MBP.attlocal.net>
2016-04-16 17:26:05 -0700

    made changes to the js function

--- public/js/scroll.js
+++ public/js/scroll.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
   $('.nav a').on('click', function(){
-    $('.btn-navbar').click(); //bootstrap 2.x
     $('.navbar-toggle').click() //bootstrap 3.x by Richard
   });
 
