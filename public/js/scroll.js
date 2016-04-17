ae8f06d7cbb437460c2a016752779764be427fba
ritesh karwa <riteshkarwa@riteshs-MBP.attlocal.net>
2016-04-16 17:52:42 -0700

    added code to navigation bar items not reload on click

--- public/js/scroll.js
+++ public/js/scroll.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
   $('.nav a').on('click', function(){
-    $('.navbar-toggle').click() //bootstrap 3.x by Richard
+    $('.navbar-toggle').click(function(e){
+        e.preventDefault();
+    }); //bootstrap 3.x by Richard
   });
 
 	$('#nav-wrapper').height($("#nav").height());
