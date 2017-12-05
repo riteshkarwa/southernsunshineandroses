0d9156d1ebaa5564b80eb0e54cf4822cbd71d3d7
ritesh <ssr@gmail.com>
2017-12-04 19:27:39 -0800

    added CORS middleware

--- index.js
+++ index.js
@@ -26,7 +26,13 @@ console.log('SMTP Configured');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
 /*Enable CORS*/
-// app.use(cors())
+app.use(cors())
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
